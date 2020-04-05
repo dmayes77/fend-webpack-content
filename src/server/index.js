d87cc50c762f7d616ab2859c0696a6bcb5746253
dmayes77 <dmayes77@gmail.com>
2020-04-05 18:41:55 -0400

    webpack is complete

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,35 @@
-var path = require('path');
+require('dotenv').config();
+
+const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const aylien = require('aylien_textapi');
+
 const mockAPIResponse = require('./mockAPI.js');
+
+// set aylien API credentias
+const aylienapi = new aylien({
+  application_id: process.env.API_ID,
+  application_key: process.env.API_KEY,
+});
 
 const app = express();
 
-app.use(express.static('src/client'));
+app.use(express.static('dist'));
 
 console.log(__dirname);
 
 app.get('/', function (req, res) {
-  res.sendFile('/client/views/index.html', { root: __dirname + '/..' });
-});
-
-// designates what port the app will listen to for incoming requests
-const port = 3000;
-app.listen(port, function () {
-  console.log(`Example app listening on port ${port}!`);
+  res.sendFile('dist/index.html');
 });
 
 app.get('/test', function (req, res) {
   res.send(mockAPIResponse);
+});
+
+// designates what port the app will listen to for incoming requests
+const port = 8081;
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
 });
